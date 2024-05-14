7927e9849544e4d5a148472f65e78ea880ca8d96
yc-02 <147283486+yc-02@users.noreply.github.com>
2024-05-14 16:00:27 -0400

    test

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const {isEmail} = require('validator');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 
 const userSchema = new Schema({
